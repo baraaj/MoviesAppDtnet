02fa37aa0bde94866d0c8e4cadf28b3d69caaaf2
baraaj <jeridibaraa@gmail.com>
2022-12-12 17:13:28 +0100

    first commit

---
+++ MovisesApp/Repositories/Abstract/IGenreService.cs
@@ -0,0 +1,15 @@
+﻿using MovisesApp.Models.Domain;
+using MovisesApp.Models.DTO;
+
+namespace MovisesApp.Repositories.Abstract
+{
+    public interface IGenreService
+    {
+       bool Add(Genre model);
+       bool Update(Genre model);
+       Genre GetById(int id);
+       bool Delete(int id);
+       IQueryable<Genre> List();
+
+    }
+}
---
+++ MovisesApp/Repositories/Abstract/IMovieService.cs
@@ -0,0 +1,16 @@
+﻿using MovisesApp.Models.Domain;
+using MovisesApp.Models.DTO;
+
+namespace MovisesApp.Repositories.Abstract
+{
+    public interface IMovieService
+    {
+       bool Add(Movie model);
+       bool Update(Movie model);
+       Movie GetById(int id);
+       bool Delete(int id);
+       MovieListVm List(string term = "", bool paging = false, int currentPage = 0);
+        List<int> GetGenreByMovieId(int movieId);
+
+    }
+}
